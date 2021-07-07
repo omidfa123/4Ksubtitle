6af8b5325201b1457c324d1d7ea0596ee0190ca3
omidfa <omidfa1234@gmail.com>
2021-07-08 00:54:32 +0430

    complit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,9 @@
+import "../styles/styles.css"
+
+// Our modules / classes
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+
+// Instantiate a new object using our modules/classes
+var mobileMenu = new MobileMenu()
+var heroSlider = new HeroSlider()
